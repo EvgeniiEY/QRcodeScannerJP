c646d36dab48ecceb0646e4ec24ee3ccfc662f9f
Evgenii Yakhontov <yakhontov.e@yandex.ru>
2024-02-01 20:09:52 +0300

    three buttons done

--- app/src/main/java/com/example/qrcodescannerjp/MainActivity.kt
+++ app/src/main/java/com/example/qrcodescannerjp/MainActivity.kt
@@ -1,67 +1,106 @@
 package com.example.qrcodescannerjp
 
+import android.graphics.drawable.shapes.Shape
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme.colors
+import androidx.compose.material.SnackbarDefaults.backgroundColor
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
 import com.example.qrcodescannerjp.ui.theme.QRcodeScannerJPTheme
+import com.example.qrcodescannerjp.ui.theme.Shapes
 import com.journeyapps.barcodescanner.ScanContract
 import com.journeyapps.barcodescanner.ScanOptions
 import com.parse.ParseObject
 
 class MainActivity : ComponentActivity() {
+    private var dksid = 0
     private val scanLauncher = registerForActivityResult(
         ScanContract()
     ) { result ->
         if (result.contents == null) {
         } else {
-        Toast.makeText(this, "QR код: ${result.contents}", Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "QR код: ${result.contents}", Toast.LENGTH_LONG).show()
+
             val secondObject = ParseObject("FirstClass")
-            secondObject.put("message","${result.contents}")
+            secondObject.put("message", "${result.contents}")
+            secondObject.put("DKSId", "${dksid++}")
             secondObject.saveInBackground {
-                if (it != null){
+                if (it != null) {
                     it.localizedMessage?.let { message -> Log.e("MainActivity", message) }
-                }else{
-                    Log.d("MainActivity","Object saved.")
+                } else {
+                    Log.d("MainActivity", "Object saved.")
                 }
             }
         }
     }
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
             QRcodeScannerJPTheme() {
-                Box(
+                Column(
                     modifier = Modifier.fillMaxSize(),
-                    contentAlignment = Alignment.Center
+                    verticalArrangement = Arrangement.SpaceAround,
+                    horizontalAlignment = Alignment.CenterHorizontally
+
                 ) {
                     Button(
+
                         onClick = {
-                        scan()
-                    }) {
-                        Text(text = "СКАН")
+                            scan()
+                        },
+                        colors = ButtonDefaults.buttonColors(backgroundColor = Color.Blue, contentColor = Color.Red)
+                    )
+                    {
+                        Text(text = "АВАРИЯ", fontSize = 20.sp)
                     }
+
+                    Button(
+
+                        onClick = {
+                            scan()
+                        },
+                        colors = ButtonDefaults.buttonColors(backgroundColor = Color.Blue, contentColor = Color.White)
+                    )
+                    {
+                        Text(text = "СКАН Запрос", fontSize = 20.sp)
+                    }
+
+                    Button(
+
+                        onClick = {
+                            scan()
+                        },
+                        colors = ButtonDefaults.buttonColors(backgroundColor = Color.Blue, contentColor = Color.White)
+                    )
+                    {
+                        Text(text = "СКАН Внести QR код в базу", fontSize = 20.sp)
+                    }
+
                 }
             }
         }
-        val firstObject = ParseObject("FirstClass")
-        firstObject.put("message","Hey ! First message from android. Parse is now connected")
-        firstObject.saveInBackground {
-            if (it != null){
-                it.localizedMessage?.let { message -> Log.e("MainActivity", message) }
-            }else{
-                Log.d("MainActivity","Object saved.")
-            }
-        }
-
-
+//        val firstObject = ParseObject("FirstClass")
+//        firstObject.put("message","Hey ! First message from android. Parse is now connected")
+//        firstObject.saveInBackground {
+//            if (it != null){
+//                it.localizedMessage?.let { message -> Log.e("MainActivity", message) }
+//            }else{
+//                Log.d("MainActivity","Object saved.")
+//            }
+//        }
     }
 
     private fun scan() {
@@ -69,68 +108,8 @@ class MainActivity : ComponentActivity() {
         options.setDesiredBarcodeFormats(ScanOptions.QR_CODE)
         options.setPrompt("Отсканируйте QR-код")
         options.setCameraId(0)
-        options.setBeepEnabled(false)
+        options.setBeepEnabled(true)
         options.setBarcodeImageEnabled(true)
         scanLauncher.launch(options)
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//        setContent {
-//            Column() {
-//                ListItem("Evgenii Yakhontov", "Programmer")
-//                ListItem("Evgenii Yakhontov", "Programmer")
-//                ListItem("Evgenii Yakhontov", "Programmer")
-//                ListItem("Evgenii Yakhontov", "Programmer")
-//                ListItem("Evgenii Yakhontov", "Programmer")
-//                ListItem("Evgenii Yakhontov", "Programmer")
-//            }
-//        }
-//    }
-//}
-//
-//@Composable
-//private fun ListItem(name:String, prof:String){
-//    Card(
-//        modifier = Modifier
-//
-//            .fillMaxWidth()
-//            .padding(10.dp),
-//        shape = RoundedCornerShape(15.dp),
-//        elevation = 5.dp
-//    ){
-//        Box() {
-//
-//            Row(){
-//                Image(painter = painterResource(id = R.drawable.mol),
-//                    contentDescription = "image",
-//                    contentScale = ContentScale.Crop,
-//                    modifier = Modifier
-//                        .padding(5.dp)
-//                        .size(64.dp)
-//                        .clip(CircleShape)
-//                )
-//                Column(modifier = Modifier.padding()) {
-//                    Text(text = name)
-//                    Text(text = prof)
-//                }
-//
-//            }
-//        }
-//    }
-//}
