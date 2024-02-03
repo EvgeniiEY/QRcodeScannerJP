cbb8b40d8c53e6ea30aa58a56a0973228d7c80a8
Evgenii Yakhontov <yakhontov.e@yandex.ru>
2024-02-04 00:01:28 +0300

    alarm in progress

--- app/src/main/java/com/example/qrcodescannerjp/MainActivity.kt
+++ app/src/main/java/com/example/qrcodescannerjp/MainActivity.kt
@@ -1,28 +1,31 @@
 package com.example.qrcodescannerjp
 
-import android.graphics.drawable.shapes.Shape
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
-import androidx.compose.material.MaterialTheme.colors
-import androidx.compose.material.SnackbarDefaults.backgroundColor
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.sp
 import com.example.qrcodescannerjp.ui.theme.QRcodeScannerJPTheme
-import com.example.qrcodescannerjp.ui.theme.Shapes
 import com.journeyapps.barcodescanner.ScanContract
 import com.journeyapps.barcodescanner.ScanOptions
 import com.parse.ParseObject
+import com.parse.ParseQuery
+
 
 class MainActivity : ComponentActivity() {
     private var dksid = 0
+
+    //put qr in base function
     private val scanLauncher = registerForActivityResult(
         ScanContract()
     ) { result ->
@@ -32,7 +35,7 @@ class MainActivity : ComponentActivity() {
 
             val secondObject = ParseObject("FirstClass")
             secondObject.put("message", "${result.contents}")
-            secondObject.put("DKSId", "${dksid++}")
+            secondObject.put("DKSId", "${dksid}")
             secondObject.saveInBackground {
                 if (it != null) {
                     it.localizedMessage?.let { message -> Log.e("MainActivity", message) }
@@ -43,9 +46,38 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+//    private val scanAlarm = doQueryAlarm(
+//        val query = ParseQuery<Parse>
+//
+//        ScanContract()
+//    ) { result ->
+//        if (result.contents == null) {
+//        } else {
+//
+//        }
+    // find number query
 
+    private val scanFinder = registerForActivityResult(
+        ScanContract()
+    ) { result ->
+        if (result.contents == null) {
+        } else {
+            Toast.makeText(this, "QR код: ${result.contents}", Toast.LENGTH_LONG).show()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+            val query = ParseQuery.getQuery<ParseObject>("FirstClass")
+
+            query.getInBackground("783WeFkftA") { Message, e ->
+                if (e == null) {
+                    Message.put("Message", "ЖОПА ХУЙ ЕБЛО !!! ")
+
+                    Message.saveInBackground()
+                }
+            }
+
+        }
+    }
+
+            override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
@@ -59,9 +91,12 @@ class MainActivity : ComponentActivity() {
                     Button(
 
                         onClick = {
-                            scan()
+                            scanAlarm()
                         },
-                        colors = ButtonDefaults.buttonColors(backgroundColor = Color.Blue, contentColor = Color.Red)
+                        colors = ButtonDefaults.buttonColors(
+                            backgroundColor = Color.Blue,
+                            contentColor = Color.Red
+                        )
                     )
                     {
                         Text(text = "АВАРИЯ", fontSize = 20.sp)
@@ -72,7 +107,10 @@ class MainActivity : ComponentActivity() {
                         onClick = {
                             scan()
                         },
-                        colors = ButtonDefaults.buttonColors(backgroundColor = Color.Blue, contentColor = Color.White)
+                        colors = ButtonDefaults.buttonColors(
+                            backgroundColor = Color.Blue,
+                            contentColor = Color.White
+                        )
                     )
                     {
                         Text(text = "СКАН Запрос", fontSize = 20.sp)
@@ -83,7 +121,10 @@ class MainActivity : ComponentActivity() {
                         onClick = {
                             scan()
                         },
-                        colors = ButtonDefaults.buttonColors(backgroundColor = Color.Blue, contentColor = Color.White)
+                        colors = ButtonDefaults.buttonColors(
+                            backgroundColor = Color.Blue,
+                            contentColor = Color.White
+                        )
                     )
                     {
                         Text(text = "СКАН Внести QR код в базу", fontSize = 20.sp)
@@ -112,4 +153,23 @@ class MainActivity : ComponentActivity() {
         options.setBarcodeImageEnabled(true)
         scanLauncher.launch(options)
     }
+
+    private fun scanAlarm() {
+        val options = ScanOptions()
+        options.setDesiredBarcodeFormats(ScanOptions.QR_CODE)
+        options.setPrompt("Отсканируйте QR-код")
+        options.setCameraId(0)
+        options.setBeepEnabled(true)
+        options.setBarcodeImageEnabled(true)
+        scanFinder.launch(options)
+    }
 }
+//        private fun doQueryAlarm() {
+//            val query = ParseQuery<ParseObject>("FirstClass")
+//            query.whereContains("DKSId", "3")
+//            val result = ParseObject("FirstClass")
+//            result.put("Alarm", "true")
+//        }
+//    }
+
+
